17a52da28dc0742292e7e11abb87919e0ebe3def
h1705 <123456@qq.com>
2020-03-19 00:56:02 +0800

    2

--- src/helper/route.js
+++ src/helper/route.js
@@ -1,24 +1,42 @@
 
 const fs = require('fs');
+const path = require('path');
 const Handlebars = require('handlebars');
 const promisify = require('util').promisify;
 const stat = promisify(fs.stat);
 const readdir = promisify(fs.readdir);
+const config = require('../config/defaultConf');
+const mime = require('./mime');
 
-// const source = fs.readFileSync()
+const tplPath = path.join(__dirname,'../template/dir.tpl');
+const source = fs.readFileSync(tplPath,'utf-8');
+const template = Handlebars.compile(source);
 
 module.exports = async function(req,res,filePath) {
     try{
         const stats = await stat(filePath);
         if(stats.isFile()) {
+            const ContentType = mime(filePath);
             res.statusCode = 200;
-            res.setHeader('Content-Type', 'text/plain');
+            res.setHeader('Content-Type', ContentType);
             fs.createReadStream(filePath).pipe(res);
         }else if(stats.isDirectory()){
             const files = await readdir(filePath);
             res.statusCode = 200;
-            res.setHeader('Content-Type', 'text/plain');
-            res.end(files.join(','));
+            res.setHeader('Content-Type', 'text/html');
+            const dir = path.relative(config.root,filePath);
+            const data = {
+                title: path.basename(filePath), //取得文件名
+                dir: dir ? `/${dir}` : '',
+                files: files.map(file => {
+                    return {
+                        file,
+                        icon: mime(file)
+                    }
+                })
+            }
+
+            res.end(template(data));
         }
     }catch (ex){
         res.statusCode = 404;
--- src/template/dir.tpl
+++ src/template/dir.tpl
@@ -4,10 +4,18 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>{{title}}</title>
+    <style>
+        body {
+            margin:30px;
+        }
+        a{
+            display: block;
+        }
+    </style>
 </head>
 <body>
     {{#each files}}
-        <a href="{{}}">{{this}}</a>
+        <a href="{{../dir}}/{{file}}">[{{icon}}]{{file}}</a>
     {{/each}}
 </body>
 </html>
